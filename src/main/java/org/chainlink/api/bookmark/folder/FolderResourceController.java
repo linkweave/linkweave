5cf5f0fec6093a2a9c7727ee8585d467bae32e41
Divid Zero <dividbzero@gmail.com>
2026-03-27 01:15:18 +0100

    feat(build): remove unused dependencies

--- src/main/java/org/chainlink/api/bookmark/folder/FolderResourceController.java
+++ src/main/java/org/chainlink/api/bookmark/folder/FolderResourceController.java
@@ -2,32 +2,23 @@ package org.chainlink.api.bookmark.folder;
 
 import java.util.List;
 
-import io.quarkus.qute.CheckedTemplate;
-import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import lombok.RequiredArgsConstructor;
 import org.chainlink.infrastructure.stereotypes.JaxResource;
 
 @JaxResource
 @RequiredArgsConstructor
-@Path("/folderlist")
+@Path("/folders")
 public class FolderResourceController {
 
     private final FolderService folderService;
 
-    @CheckedTemplate
-    public static class Templates {
-        public static native TemplateInstance folderlist(List<Folder> folders);
-    }
-
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get() {
-        var folders = folderService.getAllFolders();
-        return Templates.folderlist(folders);
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Folder> getAll() {
+        return folderService.getAllFolders();
     }
 }
